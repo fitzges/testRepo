9b6f7bed873b4fc48cde372a70235ae4257d15b4
Shawn <shawn.jess@gmail.com>
2013-01-01 20:52:04 -0500

    Added some stuff
    Extended stuff description.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ testRepo
 ========
 
 Test repo to learn how to use git
+
+How do I format this markdown stuff.
---
+++ test.bat
@@ -0,0 +1,6 @@
+@echo off
+
+REM Title		Test.bat
+REM Author		Shawn (fitz24)
+REM	Date		Jan 1 2013
+REM	Purpose		To waste some time and learn GIT.
